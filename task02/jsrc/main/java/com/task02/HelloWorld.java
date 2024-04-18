ef37109188c971fff10e8c85bade921f35ad5eaf
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-19 01:54:47 +0300

    task02 init commit

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -19,29 +19,28 @@ import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 
-
-
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true,
-//	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "hello_world-role",
+        isPublishVersion = true,
+        aliasName = "${lambdas_alias_name}",
+//        aliasName = "/home",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 
 @LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
-	}
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        System.out.println("Hello from lambda");
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("statusCode", 200);
+        resultMap.put("message", "Hello from Lambda");
+        return resultMap;
+    }
 }
