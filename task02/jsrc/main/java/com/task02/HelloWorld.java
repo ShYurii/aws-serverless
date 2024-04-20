ad4005f2626789c6090476175cf3689ee1dcc8b9
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-21 00:44:24 +0300

    task03 init commit

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,8 +3,6 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-//import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-//import com.syndicate.deployment.model.RetentionSetting;
 
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
@@ -23,7 +21,7 @@ import java.util.Map;
 @LambdaHandler(lambdaName = "hello_world",
 	roleName = "hello_world-role",
 	isPublishVersion = true,
-//	aliasName = "${lambdas_alias_name}",
+
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaUrlConfig(
