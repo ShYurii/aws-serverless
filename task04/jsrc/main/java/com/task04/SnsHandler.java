2530fa6f5a14a89b4cb0565fb3abcb82a2a45060
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-23 17:32:37 +0300

    task04 init commit

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,6 +2,7 @@ package com.task04;
 
 //import com.amazonaws.services.lambda.runtime.Context;
 //import com.amazonaws.services.lambda.runtime.RequestHandler;
+
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -30,15 +31,21 @@ import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@LambdaHandler(lambdaName = "sns_handler",
+        roleName = "sns_handler-role",
+        isPublishVersion = true,
+        aliasName = "${lambdas_alias_name}",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+
 public class SnsHandler implements RequestHandler<SNSEvent, Void> {
 
-	private static final Logger LOG = LogManager.getLogger(SnsHandler.class);
+    private static final Logger LOG = LogManager.getLogger(SnsHandler.class);
 
-	@Override
-	public Void handleRequest(SNSEvent event, Context context) {
-		for (SNSEvent.SNSRecord record : event.getRecords()) {
-			LOG.info("New message from SNS: " + record.getSNS().getMessage());
-		}
-		return null;
-	}
+    @Override
+    public Void handleRequest(SNSEvent event, Context context) {
+        for (SNSEvent.SNSRecord record : event.getRecords()) {
+            LOG.info("New message from SNS: " + record.getSNS().getMessage());
+        }
+        return null;
+    }
 }
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,6 +2,7 @@ package com.task04;
 
 //import com.amazonaws.services.lambda.runtime.Context;
 //import com.amazonaws.services.lambda.runtime.RequestHandler;
+
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -32,16 +33,22 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@LambdaHandler(lambdaName = "sqs_handler",
+        roleName = "sqs_handler-role",
+        isPublishVersion = true,
+        aliasName = "${lambdas_alias_name}",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+
 public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 
-	private static final Logger LOG = LogManager.getLogger(SqsHandler.class);
+    private static final Logger LOG = LogManager.getLogger(SqsHandler.class);
 
-	@Override
-	public Void handleRequest(SQSEvent event, Context context) {
-		for (SQSEvent.SQSMessage msg : event.getRecords()){
-			LOG.info("New message from SQS: " + msg.getBody());
-		}
-		return null;
-	}
+    @Override
+    public Void handleRequest(SQSEvent event, Context context) {
+        for (SQSEvent.SQSMessage msg : event.getRecords()) {
+            LOG.info("New message from SQS: " + msg.getBody());
+        }
+        return null;
+    }
 }
 
